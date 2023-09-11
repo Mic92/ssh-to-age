e0b3e358c8f1099452a01863c5dc81e08f02fe87
Jörg Thalheim <joerg@thalheim.io>
2023-09-11 10:33:42 +0200

    move cross-build to nix

--- flake.nix
+++ flake.nix
@@ -6,21 +6,35 @@
     flake-parts.url = "github:/hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
       systems = lib.systems.flakeExposed;
-      perSystem = { config, self', inputs', pkgs, system, ... }:  {
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
         packages = {
-          ssh-to-age = (pkgs.callPackage ./default.nix {});
+          ssh-to-age = (pkgs.callPackage ./default.nix { });
           default = config.packages.ssh-to-age;
         };
-        checks = {
-          cross-build = pkgs.runCommand "cross-build" { nativeBuildInputs = [ pkgs.gox ]; } ''
-            cp -r ${./.} .
-            chmod -R u+w .
-            gox -osarch '!darwin/386' .
-          '';
-        };
+        checks =
+          let
+            packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
+            devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
+          in
+          {
+            cross-build = self'.packages.ssh-to-age.overrideAttrs (old: {
+              nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.gox ];
+              buildPhase = ''
+                runHook preBuild
+                HOME=$TMPDIR gox -verbose -osarch '!darwin/386' ./cmd/ssh-to-age/
+                runHook postBuild
+              '';
+              doCheck = false;
+              installPhase = ''
+                runHook preBuild
+                touch $out
+                runHook postBuild
+              '';
+            });
+          } // packages // devShells;
       };
     });
 }
