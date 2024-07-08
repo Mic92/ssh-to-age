5e8ac46f99a489bb059a23e174ca8cc21ff9facd
Jörg Thalheim <joerg@thalheim.io>
2024-07-08 11:22:06 +0200

    bump version

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {}, vendorHash ? "sha256-w/REcFeH58DTQwgotxSBVR4y7aQ9rBDX2U0A4vJno7s=" }:
 pkgs.buildGoModule {
   pname = "ssh-to-age";
-  version = "1.1.7";
+  version = "1.1.8";
 
   src = ./.;
 
